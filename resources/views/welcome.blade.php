a42746148b2251867f671d2ebeb0f8d13f4f8085
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-15 13:38:39 -0500

    initial commit, 03_RoutingControllers

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel - Controllers</title>
+        <title>Laravel - Routing Controllers</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel - Controllers
+                    Laravel - Routing Controllers
                 </div>
 
                 <div class="links">
