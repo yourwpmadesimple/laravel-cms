e274a228559529dc7616b96a2f5f91086b70ee19
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-15 13:51:28 -0500

    created route post in web.php, pulled data from PostsController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,6 +14,7 @@ class PostsController extends Controller
     public function index()
     {
         //
+        return "<h1>Index Method for PostsController</h1>";
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return "Hi I am About page";
-});
+// Route::get('/about', function () {
+//     return "Hi I am About page";
+// });
 
-Route::get('/contact', function () {
-    return "Hi I am contact";
-});
+// Route::get('/contact', function () {
+//     return "Hi I am contact";
+// });
 
-Route::get(('/post/{id}/{name}'), function($id, $name) {
-    return "This is post number " . $id . "by " . $name;
-});
+// Route::get(('/post/{id}/{name}'), function($id, $name) {
+//     return "This is post number " . $id . "by " . $name;
+// });
 
-Route::get('admin/posts/example', array('as' => 'admin.home', function() {
- $url = route('admin.home');
- return "This url is " . $url;
-}));
+// Route::get('admin/posts/example', array('as' => 'admin.home', function() {
+//  $url = route('admin.home');
+//  return "This url is " . $url;
+// }));
+
+Route::get('/post', 'PostsController@index');
