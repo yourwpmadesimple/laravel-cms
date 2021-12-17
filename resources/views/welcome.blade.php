01374d012985657ad19244c7f19d705b2e95c6fb
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-16 20:28:05 -0500

    initial commit, 094_AddingColumnsToExistingColumns branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel - Drop Migration</title>
+        <title>Laravel - Adding Columns To Existing Tables With Migrate</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel - Drop Migration
+                    Laravel - Adding Columns To Existing Tables With Migrate
                 </div>
 
                 <div class="links">
