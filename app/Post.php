a25c2dbf1cb9278cf96ab8ae1f25f8b42c7fa0dd
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-17 19:19:01 -0500

    final

---
+++ app/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+// If using a class name other than the the name of the table that is in your database, then do the following
+    /**
+     * Example: 
+     * class PostAdmin extends Model {
+     * 
+     *  // define the name of the actual column name in the database table:
+     * example 1: protected $table = 'posts' (posts is the name of the table in the database)
+     * example 2: protected $primaryKey = 'post_id' (this is defined in the case the primary key is named 'id')
+     * }
+     * 
+     * 
+     */
+
+class Post extends Model
+{
+
+    
+    
+}
--- routes/routes.php
+++ routes/routes.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use App\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,35 +19,67 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*
+|--------------------------------------------------------------------------
+|Database Raw SQL Queries
+|--------------------------------------------------------------------------
+|
+*/
+
 /** Inserting Data to the Database **/
 
-Route::get('/create', function() {
-    DB::insert('insert into posts(title, content)
-    values
-        ("React JS", "A JavaScript library for building user interfaces"),
-        ("PHP", "PHP is a popular general-purpose scripting language that is especially suited to web development."),
-        ("Laravel", "Laravel is a web application framework with expressive, elegant syntax.")'
-    );
-});
+// Route::get('/create', function() {
+//     DB::insert('insert into posts(title, content)
+//     values
+//         ("React JS", "A JavaScript library for building user interfaces"),
+//         ("PHP", "PHP is a popular general-purpose scripting language that is especially suited to web development."),
+//         ("Laravel", "Laravel is a web application framework with expressive, elegant syntax.")'
+//     );
+// });
 
-/** Reading Data from the Database **/
-Route::get('/read', function(){
-    $results = DB::select('select * from posts where id > 0');
-    foreach($results as $post){
-        echo "<h1>$post->title</h1>
-        <hr/>
-        <p>$post->content</p>";
+// /** Reading Data from the Database **/
+// Route::get('/read', function(){
+//     $results = DB::select('select * from posts where id > 0');
+//     foreach($results as $post){
+//         echo "<h1>$post->title</h1>
+//         <hr/>
+//         <p>$post->content</p>";
+//     }
+// });
+
+// /** Updating Data in the Database **/
+// Route::get('/update', function() {
+//     $update = DB::update('update posts set title = "Laravel" where id = ?', [1]);
+//     return $update;
+// });
+
+// /** Deleting Data from the Database **/
+// Route::get('/delete', function() {
+//     $delete = DB::delete('delete from posts where id > 0');
+//     return $delete;
+// });
+
+/*
+|--------------------------------------------------------------------------
+|ELOQUENT ORM (Object Relational Model)
+|--------------------------------------------------------------------------
+*/
+
+Route::get('eloquent/read', function() {
+    // Retrieve information from the database
+    header('Content-Type: application/json');
+    $posts = Post::all();
+
+    foreach($posts as $post) {
+        echo '<pre>';
+        echo json_encode(json_decode($post), JSON_PRETTY_PRINT);
+        echo '</pre>';
     }
 });
 
-/** Updating Data in the Database **/
-Route::get('/update', function() {
-    $update = DB::update('update posts set title = "Laravel" where id = ?', [1]);
-    return $update;
-});
 
-/** Deleting Data from the Database **/
-Route::get('/delete', function() {
-    $delete = DB::delete('delete from posts where id > 0');
-    return $delete;
+Route::get('eloquent/find', function(){
+    $posts = Post::find(1);
+
+    return $posts;
 });
